ad07cfad5bcd62cf5d21f2996132452cc0bab7eb
alf12300 <131310389+alf12300@users.noreply.github.com>
2023-08-31 22:14:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,10 @@ elif tab_selection == "Estimador de Costos":
     
     # Add Custom Tool button in the right column
     with col_button:
+        st.markdown("<br><br>", unsafe_allow_html=True)
         if st.button("Agregar Herramienta Personalizada"):
             st.session_state.custom_tools.append({"name": "", "unit_price": 0.0, "quantity": 0})
+
 
     # Retrieve tools for the selected service
     tools_for_service = TOOLS_MAPPING.get(selected_service, [])
