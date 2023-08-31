eb29eaa0e1e418edb0941af6b2a18d376156dda7
alf12300 <131310389+alf12300@users.noreply.github.com>
2023-08-31 22:33:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ elif tab_selection == "Estimador de Costos":
     # Add Custom Tool button in the right column
     with col_button:
         st.markdown("<br>", unsafe_allow_html=True)
-        if st.button("Agregar Herramienta Personalizada"):
+        if st.button("Agregar Costo"):
             st.session_state.custom_tools.append({"name": "", "unit_price": 0.0, "quantity": 0})
 
 
