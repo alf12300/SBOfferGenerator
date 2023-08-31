a4d8b856c67f068c8915d3a066d0c4bdf95cf262
alf12300 <131310389+alf12300@users.noreply.github.com>
2023-08-31 20:45:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,9 @@ if tab_selection == "Generador de Propuestas":
     
     
     # Title Row
-    spacer_left, title_col, spacer_right = st.columns([1,6,1])
-    title_col.title("Generador de Ofertas")
-    
-        
+    title("Generador de Ofertas")
     # Create spacer and main columns for the top row
-    spacer_col1, col1, col2, spacer_col2 = st.columns([1,3,3,1])
+    col1, col2= st.columns([6,6])
     
     # Column 1: Customer Data
     with col1:
@@ -89,43 +86,34 @@ if tab_selection == "Generador de Propuestas":
             st.session_state.added_services.append(service_data)
             
     # Create spacer and main column for the bottom row
-    spacer_col3, col3, spacer_col4 = st.columns([1,6,1])
     
-    # Column 3: List of Services Added to Quote
-    # Column 3: List of Services Added to Quote
-    with col3:
-        st.markdown("---")
-        st.subheader("Servicios agregados a la oferta")
-        for idx, service_data in enumerate(st.session_state.added_services):
-            service_name = service_data['name']
-            st.markdown(f"### {service_name}")
-            st.write(f"Descripción: {service_data['description']}")
-            st.write(f"Costo: €{service_data['cost']:.2f}")
-    
-            # Add a "Remove" button for each service
-            if st.button(f"Eliminar {service_name}", key=f"remove_{idx}"):
-                # If button is pressed, remove the service from the list
-                st.session_state.added_services = [service for i, service in enumerate(st.session_state.added_services) if i != idx]
-                st.experimental_rerun()  # Rerun the app to refresh the list
+    st.markdown("---")
+    st.subheader("Servicios agregados a la oferta")
+    for idx, service_data in enumerate(st.session_state.added_services):
+        service_name = service_data['name']
+        st.markdown(f"### {service_name}")
+        st.write(f"Descripción: {service_data['description']}")
+        st.write(f"Costo: €{service_data['cost']:.2f}")
+
+        # Add a "Remove" button for each service
+        if st.button(f"Eliminar {service_name}", key=f"remove_{idx}"):
+            # If button is pressed, remove the service from the list
+            st.session_state.added_services = [service for i, service in enumerate(st.session_state.added_services) if i != idx]
+            st.experimental_rerun()  # Rerun the app to refresh the list
     
     
     # Generate quote button
-    spacer_button, button_col, _, _ = st.columns([1,3,3,1])
-    with button_col:
-        total_cost = calculate_cost(st.session_state.added_services)
-        st.markdown(f"## Costo Total: **€{total_cost:.2f}**")
-        
-        
-        
+    total_cost = calculate_cost(st.session_state.added_services)
+    st.markdown(f"## Costo Total: **€{total_cost:.2f}**")
+            
     # Edit Commercial Terms
-    spacer_terms, terms_col, _ = st.columns([1,6,1])
-    with terms_col:
-        st.markdown("---")
-        st.subheader("Términos Comerciales")
-        edited_terms = st.text_area("Editar términos comerciales:", value=COMMERCIAL_TERMS, height=150)
+
+    st.markdown("---")
+    st.subheader("Términos Comerciales")
+    edited_terms = st.text_area("Editar términos comerciales:", value=COMMERCIAL_TERMS, height=150)
     st.markdown("---")
     # Create a row with two centered columns and spacers on the side
-    spacer_left, generate_col, download_col, spacer_right = st.columns([1, 3, 3, 1])
+    generate_col, download_col = st.columns([6,6])
     
     # "Generar Oferta" button in the left column
     if generate_col.button("Generar Oferta", use_container_width=True):
