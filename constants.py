e74286907a34830693a8e781cef93092bd2b04e5
alf12300 <131310389+alf12300@users.noreply.github.com>
2023-09-02 16:51:35 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -32,29 +32,9 @@ COSTS_DESCRIPTIONS = {
 
 # Commercial terms 
 COMMERCIAL_TERMS = '''Términos Comerciales:
-Recargos:
-Todos los servicios están sujetos a un recargo del 10% por concepto de servicio.
-Cotización: Cualquier requerimiento adicional fuera de la cotización inicial podría generar cargos extra.
-Disponibilidad:
-El inicio de los trabajos está sujeto a disponibilidad y programación. Se recomienda reservar con anticipación para garantizar las fechas deseadas.
-Pago:
-El pago debe realizarse dentro de los 30 días posteriores a la finalización del servicio.
-Aceptamos pagos a través de transferencia bancaria, cheque o efectivo.
-Para proyectos que excedan un monto de €5,000, se requiere un adelanto del 50% para comenzar los trabajos.
-Cancelaciones:
-Las cancelaciones hechas con menos de 10 días de anticipación pueden estar sujetas a una tarifa de cancelación equivalente al 10% del costo total del proyecto.
-Las postergaciones o reprogramaciones deben ser comunicadas con al menos 5 días de anticipación.
-Garantía:
-Todos nuestros trabajos de remodelación cuentan con una garantía de 12 meses, asegurando la calidad y durabilidad de los mismos.
-Seguridad:
-Todos nuestros empleados están asegurados y capacitados para garantizar la seguridad en cada proyecto.
-
-Información de Contacto:
-Nombre de la Empresa: Remodelaciones XYZ
-Dirección: Calle Falsa 123, Ciudad, Provincia
-Teléfono: +34 123 456 789
-Email: contacto@remodelacionesxyz.es
-Horario de Atención: Lunes a Viernes de 09:00 a 18:00'''
+- Sin crédito, presupuesto válido por 30 días 			
+- Horario de trabajo: 9h - 14h / 15h - 18h. Días de trabajo: acordar con cliente
+- Informar si existe horario especial de obras en la comunidad.	'''
 
 # Mapping of services to their associated tools
 TOOLS_MAPPING = {
